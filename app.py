aaddc13faa5ce0a58d649edab9f9e08b3d66db8f
Chethan <chethansputran222@gmail.com>
2021-05-26 20:50:02 +0530

    geocoder added

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from send_email import sendEmail
 from sqlalchemy.sql import func
 from werkzeug.utils import secure_filename
+import pandas as pd
+from geopy.geocoders import Nominatim
+from datetime import datetime
 
+file_name = ""
 app = Flask(__name__)
 
 #Configuring the database
@@ -57,29 +61,45 @@ def success():
         else:
             # return render_template('failure.html')
             return render_template('index.html',text="Sorry!Email has been taken!")
-       
-
+          
 
 #Uploading file
-@app.route('/upload')
+@app.route('/upload',methods=['POST','GET'])
 def upload():
     if request.method == 'POST':
-        global file
+        global file_name
         file = request.files['inFile']
-        contents=file.read()
         
-        #Saving file
-        file.save(secure_filename("Uploaded_file_"+file.filename))
-        # print(contents)
-        
-        #Implementing download button
-        return render_template('fileSuccess.html',btn="dn.html")
-        #return render_template('index.html',btn="dn.html")
+        try:
+            
+            #contents=file.read()
+            df = pd.read_csv(file)  
+            
+            gc  = Nominatim(user_agent="http")
+            df["coordinates"] = df["Address"].apply(gc.geocode)
+            df['Latitude'] = df['coordinates'].apply(lambda x:x.latitude if x!= None else None)
+            df['Longitude'] = df['coordinates'].apply(lambda x:x.longitude if x!= None else None)
+            df = df.drop("coordinates",1)
+            
+            file_name = datetime.now().strftime("uploads/%Y-%m-%d-%H-%M-%S-%f"+".csv")
+            df.to_csv(file_name,index=None)
+            #Saving file
+            #file.save(secure_filename("Uploaded_file_"+file.filename))
+            # print(contents)
+            
+            #Implementing download button
+            return render_template('fileSuccess.html',text=df.to_html(),btn="dn.html")
+            #return render_template('index.html',btn="dn.html")
+        except Exception as e:
+            print(e)
+            return render_template("un.html",text="File should have Address column")
+    return render_template("un.html") 
+    
        
 #Downloading file
-@app.route('/download',methods=['POST'])
+@app.route('/download',methods=['POST','GET'])
 def download():
-    return send_file("Uploaded_file_"+file.filename,attachment_filename="uploadedfile",as_attachment=True)
+    return send_file(file_name,attachment_filename="uploadedfile",as_attachment=True)
   
 if __name__ == '__main__':
     app.debug=True
